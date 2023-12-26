a533a36bd60d8d9cdc409520151f99e51aade9e7
FaruqAlfa <alfafahmi172@gmail.com>
2023-12-26 14:15:48 +0700

    Update

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -73,7 +73,7 @@ class ProfilePage extends StatelessWidget {
                     );
                   }
                 },
-              ),
+              ), 
             ),
 
             Container(
