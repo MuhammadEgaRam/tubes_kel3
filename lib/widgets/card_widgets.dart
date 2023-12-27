8a435a049c8016db5d6628d63994e708e8d44619
bryantosin1 <bryantosin1@gmail.com>
2023-12-27 12:53:29 +0700

    history

--- lib/widgets/card_widgets.dart
+++ lib/widgets/card_widgets.dart
@@ -72,29 +72,6 @@ class ItemCard extends StatelessWidget {
               color: Color.fromARGB(255, 0, 0, 0),
             ),
           ),
-          subtitle: Row(
-            children: [
-              Text(
-                item.tanggalsc,
-                style: SafeGoogleFont(
-                  'Urbanist',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-              ),
-              SizedBox(width: 20),
-              Text(
-                item.waktusc,
-                style: SafeGoogleFont(
-                  'Urbanist',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-              ),
-            ],
-          ),
           trailing: GestureDetector(
             onTap: () {
               _showDetailDialog(context);
@@ -123,41 +100,38 @@ class ItemCard extends StatelessWidget {
       builder: (BuildContext context) {
         return AlertDialog(
           content: Container(
-            width: 297,
-            height: 560,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: Text(
-                        'Detail',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+            height: 560, // Set a specific height for the dialog
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                          'Detail',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 8),
-                buildInfoRow('ID', '0'),
-                buildInfoRow('NIM', '2141720143'),
-                buildInfoRow('Nama', 'Muhammad Ega Rama Fernanda'),
-                buildInfoRow('Tempat/Tgl Lahir', 'Malang, 12-07-2002'),
-                buildInfoRow('Prodi', 'D-IV T. Informatika'),
-                buildInfoRow('Alamat', 'JL. Mawar NO.39'),
-                buildInfoRow('Kecamatan', '1/3 Penataban Giri'),
-                buildInfoRow('Kabupaten', 'Banyuwangi'),
-              ],
+                  SizedBox(height: 8),
+                  buildInfoRow('ID', '0'),
+                  buildInfoRow('NIM', '2141720143'),
+                  buildInfoRow('Nama', 'Muhammad Ega Rama Fernanda'),
+                  buildInfoRow('Tempat/Tgl Lahir', 'Malang, 12-07-2002'),
+                  buildInfoRow('Prodi', 'D-IV T. Informatika'),
+                  buildInfoRow('Alamat', 'JL. Mawar NO.39'),
+                  buildInfoRow('Kecamatan', '1/3 Penataban Giri'),
+                  buildInfoRow('Kabupaten', 'Banyuwangi'),
+                ],
+              ),
             ),
           ),
         );
